c5065c744de2ff923b2dd4a7369ffade2cff12e6
Ruha Bashir <ruha.bashir@stockport.gov.uk>
2020-06-26 13:27:59 +0100

    cop code

--- COPStarter/COPMethods.cs
+++ COPStarter/COPMethods.cs
@@ -1,38 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace COPStarter
 {
 	public class COPMethods
 	{
-		public int calculateSum(int number1, int number2)
-		{
-			return number1 + number2;
-		}
-
-		public int countVowels(string Sentence)
-		{
-			string[] letters = Sentence.Split("");
-			int vowelCount = 0;
-
-			foreach (var item in Sentence)
-			{
-				if(item.ToString() == "a" || item.ToString() == "e" || item.ToString() == "i" ||  item.ToString() == "o" || item.ToString() == "u" )
-				{
-					vowelCount = vowelCount + 1;
-				}
-			}
-
-			return vowelCount;
-		}
-
-		public int milesToKM(int Miles)
-		{
-			double km = Miles * 1.609344;
-
-			return Convert.ToInt32(km);
-		}
-
-		public bool isItEqualSlces(int totalSlices, int numberOfRecipients, int slicesEach)
+		public bool SliceOfPie(int totalSlices, int numberOfRecipients, int slicesEach)
 		{
 			try
 			{
@@ -49,6 +24,138 @@ namespace COPStarter
 			{
 				return false;
 			}
+		}
+
+		public string ShuffleTheName(string firstNameAndLastName)
+		{
+			string[] result = firstNameAndLastName.Split(' ');
+
+			return result[1] + " " + result[0];
+		}
+
+		public int CountSyllables(string word)
+		{
+			return word.Split('-').Length;
+		}
+
+		public bool XsAndOs(string xAndO)
+		{
+			var a = xAndO.Count(c => c == 'x' || c == 'X');
+
+			var b = xAndO.Count(c => c == 'o' || c == 'O');
+
+			if (a == b)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		public bool StrangePair(string one, string two)
+		{
+			if (string.IsNullOrEmpty(one) || string.IsNullOrEmpty(two))
+			{
+				return one == two;
+			}
+
+			return one.First() == two.Last() && two.First() == one.Last();
+		}
+
+		public bool PrimeNumber(int number)
+		{
+			int i = 2;
+			while (i++ <= number && number % i != 0) ;
+			return i == number;
+		}
+
+		public int[] RemoveLowest(int[] array)
+		{
+			List<int> list = array.ToList();
+			if (list.Count != 0)
+			{
+				list.Remove(list.Min());
+			}
+
+			return list.ToArray();
+		}
+
+		public string ConvertZeroAndOne(string words)
+		{
+			words = words.ToLower();
+			words = Regex.Replace(words, "one", "1");
+			words = Regex.Replace(words, "zero", "0");
+			words = Regex.Replace(words, " ", "");
+			words = Regex.Replace(words, "\\D", "");
+
+            if (words.Length % 8 != 0)
+            {
+				words = words.Remove(words.Length - (words.Length % 8));
+            }
+
+            return words;
+		}
+
+		public int SockPairs(string socks)
+		{
+			return socks.ToCharArray().GroupBy(c => c).Select(c => c.Count() / 2).Sum();
+		}
+
+		public int PowerRanger(int power, int min, int max)
+		{
+			int counter = 0;
+			for (int i = min; i <= max; i++)
+			{
+				if (Math.Pow(i, 1.0 / power) % 1 == 0) counter++;
+			}
+			return counter;
+		}
+
+		public bool SmoothSentences(string sentence)
+		{
+			return Regex.Matches(sentence, @"([a-z]) (?!\1)", RegexOptions.IgnoreCase).Count == 0;
+		}
+
+		public bool ParselTongue(string sentence)
+		{
+			return sentence.ToLower().Split().Count(x => x.Contains("s") && !x.Contains("ss")) == 0;
+		}
+
+		public string TwelveVs24(string time)
+		{
+			if (time.Contains("am") || time.Contains("pm"))
+			{
+				return DateTime.Parse(time).ToString("H:mm");
+			}
+
+			return DateTime.Parse(time).ToString("h:mm tt").ToLower();
+		}
+
+		public bool SameLetterPatterns(string wordOne, string wordTwo)
+		{
+			if (wordOne.Length != wordTwo.Length) return false;
+			for (int i = 0; i < wordOne.Length; i++)
+			{
+				if (wordOne.IndexOf(wordOne.Substring(i, 1), i + 1) != wordTwo.IndexOf(wordTwo.Substring(i, 1), i + 1)) return false;
+			}
+			return true;
+		}
+
+		public bool PalindromeDescendent(int number)
+		{
+			var temp = number.ToString();
+			for (int i = 0; i < temp.Length; i++)
+			{
+				if (temp.Length > 4)
+				{
+					if (Convert.ToInt32(temp[0].ToString()) + Convert.ToInt32(temp[1].ToString()) <= 9)
+						temp = (Convert.ToInt32(temp[0].ToString()) + Convert.ToInt32(temp[1].ToString())).ToString() + temp.Substring(2, temp.Length - 4) + (Convert.ToInt32(temp[temp.Length - 1].ToString()) + Convert.ToInt32(temp[temp.Length - 2].ToString())).ToString();
+					else
+						return false;
+				}
+				if (temp.Length <= 4 && temp.Length > 2)
+					temp = (Convert.ToInt32(temp[0].ToString()) + Convert.ToInt32(temp[1].ToString())).ToString() + (Convert.ToInt32(temp[temp.Length - 1].ToString()) + Convert.ToInt32(temp[temp.Length - 2].ToString())).ToString();
+			}
+			return temp[0] == temp[1];
 		}
 	}
 }
--- XUnitTestProject1/COPTests.cs
+++ XUnitTestProject1/COPTests.cs
@@ -1,6 +1,5 @@
 using Xunit;
 using COPStarter;
-using System.Reflection.Metadata;
 
 namespace XUnitTestProject1
 {
@@ -8,55 +7,273 @@ namespace XUnitTestProject1
 	{
 		COPMethods cs = new COPMethods();
 
-		// you need to add 2 numbers together,
-		// 2+2 = 4 
-		// 2+3 != 4
-		[Fact]
-		public void Add_two_numbers()
-		{
-			int number1 = 2;
-			int number2 = 4;
-			int sum;
-
-			sum = cs.calculateSum(number1, number2);
-
-			Assert.Equal(6, sum);
-		}
-
-		[Theory]
-		[InlineData("aeiou", 5)]
-		[InlineData("this is an other test and it has some cowels in it", 15)]
-		public void Count_vowels_in_a_sentance(string input, int vowelCount)
-		{
-			string sentence = input;
-			int sum;
-
-			sum = cs.countVowels(sentence);
-
-			Assert.Equal(vowelCount, sum);
-		}
-
-		[Theory]
-		[InlineData(6, 10)]
-		[InlineData(60, 97)]
-		public void convert_miles_to_the_nearest_km(int miles, int km)
-		{
-			int answerKm = cs.milesToKM(miles);
-
-			Assert.Equal(km, answerKm);
-		}
-
+		// Challenge 1 - Slice of Pie
+		// You need to create a function that determines whether or not it's possible to split a pie fairly given 3 parameters
+		// Paramters are Total number of slices, number of recipients, how many slices each person gets
+		// Return true if slices can be distributed evenly - E.g. (11, 5, 2) => true 
+		// Return false if slices cannot be distributed evenly - E.g. (11, 5, 3) => false
+		// Return true if there are zero people
 		[Theory]
 		[InlineData(10, 5, 2, true)]
 		[InlineData(100, 5, 2, true)]
 		[InlineData(10, 9, 2, false)]
 		[InlineData(4, 3, 2, false)]
 		[InlineData(4, 0, 2, true)]
-		public void check_even_slices(int totalSlices, int numberOfRecipients, int slicesEach, bool outcome)
+		public void Slice_Of_Pie(int totalSlices, int numberOfRecipients, int slicesEach, bool outcome)
 		{
-			bool result = cs.isItEqualSlces(totalSlices, numberOfRecipients, slicesEach);
+			bool result = cs.SliceOfPie(totalSlices, numberOfRecipients, slicesEach);
 
 			Assert.Equal(outcome, result);
+		}
+
+		// Challenge 2 - Shuffle the name
+		// You need to create a function that takes a string (of a person's first and last name)
+		// Return the string with first and last name swapped 
+		// E.g. "Ruha Bashir" => "Bashir Ruha"
+		[Theory]
+		[InlineData("Ruha Bashir", "Bashir Ruha")]
+		[InlineData("Seymour Butts", "Butts Seymour")]
+		[InlineData("James Sam", "Sam James")]
+		[InlineData("Donald Trump", "Trump Donald")]
+
+		public void Shuffle_The_Name(string names, string answer)
+		{
+			var result = cs.ShuffleTheName(names);
+
+			Assert.Equal(answer, result);
+		}
+
+		// Challenge 3 - Count Syllables
+		// You need to create a function that counts the number of syllables a word has
+		// Each syllable is separated with a dash -
+		// Syllable is a word separated by dash -
+		// Return number of syllables
+		// E.g. "buf-fet" => 2
+		[Theory]
+		[InlineData("buf-fet", 2)]
+		[InlineData("beau-ti-ful", 3)]
+		[InlineData("mon-u-men-tal", 4)]
+		[InlineData("on-o-mat-o-poe-ia", 6)]
+		public void Count_Sylabbles(string word, int answer)
+		{
+			var result = cs.CountSyllables(word);
+
+			Assert.Equal(answer, result);
+		}
+
+		// Challenge 4 - Xs and Os, Nobody Knows 
+		// You need to create a function that takes a string and checks if it has same number of x's and o's
+		// Return true if same number E.g. ("ooxx") => true
+		// Return false if not same number E.g. ("xooxx") => false
+		// Return true if there are no x and o
+		// Must be case insensitive
+		[Theory]
+		[InlineData("ooxx", true)]
+		[InlineData("xooxx", false)]
+		[InlineData("ooxXm", true)]
+		[InlineData("zpzpzp", true)]
+		[InlineData("zzoo", false)]
+		public void X_And_O(string xAndO, bool answer)
+		{
+			var result = cs.XsAndOs(xAndO);
+
+			Assert.Equal(answer, result);
+		}
+
+		// Challenge 5 - Strange pair
+		// You need to create a function that checks whether a pair of strings is strange pair or not
+		// A pair of strings is a strange pair if both are the following are true:
+		// 1) The 1st string's first letter = 2nd string's last letter
+		// 2) The 1st string's last letter = 2nd string's first letter
+		// Return true is it is a strange pair - E.g. ("ratio", "orator") => true
+		// Return false if it is not a strange pair - E.g. ("bush", "hubris") => false
+		// It should work on a pair of empty strings 
+		[Theory]
+		[InlineData("ratio", "orator", true)]
+		[InlineData("sparkling", "groups", true)]
+		[InlineData("bush", "hubris", false)]
+		[InlineData("", "", true)]
+		public void Strange_Pair(string one, string two, bool answer)
+		{
+			var result = cs.StrangePair(one, two);
+
+			Assert.Equal(answer, result);
+		}
+
+		// Challenge 6 - Prime number
+		// You need to create a function that outputs true if a number is prime, and false otherwise
+		// A prime number has no other factors except 1 and itself
+		// 1 is not considered a prime number
+		[Theory]
+		[InlineData(31, true)]
+		[InlineData(18, false)]
+		[InlineData(11, true)]
+		public void Prime_Number(int number, bool answer)
+		{
+			var result = cs.PrimeNumber(number);
+
+			Assert.Equal(answer, result);
+		}
+
+		// Challenge 7 - The  Museum of incredibly dull things
+		// You need to create a function that takes in an array of integers and removes the smallest one
+		// Return the array without the smallest number E.g. ([1, 2, 3, 4, 5] ) => [2, 3, 4, 5]
+		// Don't change the order of array
+		// If array is empty then return empty array
+		// If there are multiple items with the same value, remove item with lower index E.g. ([2, 2, 1, 2, 1]) => [2, 2, 2, 1]
+		[Theory]
+		[InlineData(new[] { 1, 2, 3, 4, 5 }, new [] {2, 3, 4, 5})]
+		[InlineData(new[] { 5, 3, 2, 1, 4 }, new[] { 5, 3, 2, 4 })]
+		[InlineData(new[] { 2, 2, 1, 2, 1 }, new[] { 2, 2, 2, 1 })]
+		[InlineData(new[] { 1, 2, 3, 4, 5, 6 }, new[] { 2, 3, 4, 5, 6 })]
+		public void Remove_Lowest(int [] array, int [] answer)
+		{
+			var result = cs.RemoveLowest(array);
+
+			Assert.Equal(answer, result);
+		}
+
+		// Challenge 8 - Convert “Zero” and “One” to “1” and “0” 
+		// You need to create a function that takes a string as an argument
+		// Return a string containing 1s and 0s based on the string argument's words
+		// If any word in the argument is not equal to "zero" or "one" (case insensitive), you should ignore it
+		// The returned string's length should be a multiple of 8, if the string is not a multiple of 8 you should remove the numbers in excess
+		[Theory]
+		[InlineData("zero one zero one zero one zero one", "01010101")]
+		[InlineData("Zero one zero ONE zero one zero one", "01010101")]
+		[InlineData("zero one zero one zero one zero one one two", "01010101")]
+		[InlineData("zero one zero one zero one zero three", "")]
+		[InlineData("one one", "")]
+		public void ConvertZeroAndOne(string words, string answer)
+		{
+			var result = cs.ConvertZeroAndOne(words);
+
+			Assert.Equal(answer, result);
+		}
+
+		// Challenge 9 - Sock pairs
+		// Joseph is in the middle of packing for a vacation
+		// He's having trouble finding all his socks
+		// You need to create a function that returns the number of sock pairs he has
+		// A sock pair consists of two of the same letter - E.g. AA
+		// The socks are represented as an unordered sequence
+		// If given an empty string (no socks in the drawer), return 0.
+		// There can be multiple pairs of the same type of sock, such as two pairs of CC
+		[Theory]
+		[InlineData("AA", 1)]
+		[InlineData("ABABC", 2)]
+		[InlineData("CABBACCC", 4)]
+		public void Sock_Pairs(string socks, int answer)
+		{
+			var result = cs.SockPairs(socks);
+
+			Assert.Equal(answer, result);
+		}
+
+		// Challenge 10 - Power ranger
+		// You need to create a function that takes in n, a, b and returns the number of values raised to the nth power that lie in the range [a, b], inclusive
+		// Remember that the range is inclusive
+		// a < b will always be true
+		[Theory]
+		[InlineData(2, 49, 65, 2)]
+		[InlineData(3, 1, 27, 3)]
+		[InlineData(10, 1, 5, 1)]
+		[InlineData(5, 31, 33, 1)]
+		[InlineData(4, 250, 1300, 3)]
+		public void Power_Ranger(int power, int min, int max, int answer)
+		{
+			var result = cs.PowerRanger(power, min, max);
+
+			Assert.Equal(answer, result);
+		}
+
+		// Challenge 11 - Smooth Sentences
+		// You need create a function that takes a sentence as an argument and checks whether that is a smooth sentence or not
+		// A smooth sentence is one where the last letter of each word is ideantical to the first letter of the following word
+		// Return true if sentence is smooth
+		// Return false if sentence is not smooth
+		// E.g. "Carlos swam masterfully"
+		[Theory]
+		[InlineData("Marta appreciated deep perpendicular right trapezoids", true)]
+		[InlineData("Someone is outside the doorway", false)]
+		[InlineData("She eats super righteously", true)]
+		public void Smooth_Sentences(string sentence, bool answer)
+		{
+			var result = cs.SmoothSentences(sentence);
+
+			Assert.Equal(answer, result);
+		}
+
+		// Challenge 12 - Parseltongue      
+		// Hermione has come up with a precise formula for determining whether or not a phrase was ssspoken by a parssseltongue
+		// You need to create a function that takes in a sentence and determines whether the sentence is parsel tongue or not
+		// Each word in a sssentence must contain either:
+		// 1) At least 2 instances of the letter "s" - E.g. must be together ss
+		// 2) Zero instances of the letter "s"
+		// Return true if sentence is parsel tongue
+		// Return false if sentence is not parsel tongue
+		// There should be no words with only one instance of the letter "s" - E.g. She ssselects to eat that apple
+		[Theory]
+		[InlineData("Sshe ssselects to eat that apple", true)]
+		[InlineData("She ssselects to eat that apple", false)]
+		[InlineData("Beatrice samples lemonade", false)]
+		[InlineData("You ssseldom sssspeak sso boldly, ssso messmerizingly", true)]
+
+		public void Parsel_Tongue(string sentence, bool answer)
+		{
+			var result = cs.ParselTongue(sentence);
+
+			Assert.Equal(answer, result);
+		}
+
+		// Challenge 13 - 12 vs 24 Hours
+		// You need to create a function that converts 12-hour time to 24-hour time or vice versa
+		// Return the output as a string
+		// A 12-hour time input will be denoted with an am or pm suffix
+		// A 24-hour input time contains no suffix
+		[Theory]
+		[InlineData("12:00 am", "0:00")]
+		[InlineData("6:20 pm", "18:20")]
+		[InlineData("21:00", "9:00 pm")]
+		[InlineData("5:05", "5:05 am")]
+		public void Twelve_Vs_24(string time, string answer)
+		{
+			var result = cs.TwelveVs24(time);
+
+			Assert.Equal(answer, result);
+		}
+
+		// Challenge 14 - Same letter patterns
+		// You need to create a function that returns true if two strings share the same letter pattern, and false otherwise
+		// E.g. ("ABAB", "CDCD") => true
+		[Theory]
+		[InlineData("ABAB", "CDCD", true)]
+		[InlineData("ABCBA", "BCDCB", true)]
+		[InlineData("FFGG", "CDCD", false)]
+		[InlineData("FFFF", "ABCD", false)]
+		public void Same_Letter_Patterns(string wordOne, string wordTwo, bool answer)
+		{
+			var result = cs.SameLetterPatterns(wordOne, wordTwo);
+
+			Assert.Equal(answer, result);
+		}
+
+		// Challenge 15 - Palindrome Descendant (Bonus)
+		// You need to create a function that takes an integer and returns true if the number itself is a palindrome or any of its descendants down to 2 digits (a 1-digit number is trivially a palindrome)
+		// A number may not be a palindrome, but its descendant can be
+		// A number's direct child is created by summing each pair of adjacent digits to create the digits of the next number
+		// For instance, 123312 is not a palindrome, but its next child 363 is, where: 3 = 1 + 2; 6 = 3 + 3; 3 = 1 + 2
+		// Numbers will always have an even number of digits
+		[Theory]
+		[InlineData(11211230, false)]
+		[InlineData(13001120, true)]
+		[InlineData(23336014, true)]
+		[InlineData(11, true)]
+		public void Palindrome_Descendant(int number, bool answer)
+		{
+			var result = cs.PalindromeDescendent(number);
+
+			Assert.Equal(answer, result);
 		}
 	}
 }
